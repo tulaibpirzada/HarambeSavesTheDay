bed1ee6ff51ea24be7358c7ccbadede005e00480
TAHOOR\HP <tahoor.pirzada@gmail.com>
2016-09-24 17:30:39 +0500

    Add Gorilla Walking

---
+++ Assets/Scripts/GorillaMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    private Animator gorillaAnim;
+
+	// Use this for initialization
+	void Start () {
+        gorillaAnim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float input_x = Input.GetAxisRaw("Horizontal");
+
+        bool isWalking = (Mathf.Abs(input_x)) > 0;
+        gorillaAnim.SetBool("isWalking",isWalking);
+        if (isWalking)
+        {
+            gorillaAnim.SetFloat("x", input_x);
+        }
+
+        transform.position += new Vector3(input_x, 0, 0).normalized*Time.deltaTime;
+
+    }
+}
