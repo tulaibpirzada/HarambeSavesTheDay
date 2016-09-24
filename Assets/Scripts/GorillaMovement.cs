ab16e0c2cab38da2f845eac3fa42eafb08bdda0b
TAHOOR\HP <tahoor.pirzada@gmail.com>
2016-09-24 18:33:47 +0500

    Correct Gorilla Walking Transition

--- Assets/Scripts/GorillaMovement.cs
+++ Assets/Scripts/GorillaMovement.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class GorillaMovement : MonoBehaviour {
 
     private Animator gorillaAnim;
 
