d0800f96e97dd3c22a525e474985e575e6ab3fc2
Tulaib <tulaib.pirzada@gmail.com>
2016-09-28 12:41:14 +0500

    Initial work on touch detection

--- Assets/Scripts/Controllers/GorillaMovementController.cs
+++ Assets/Scripts/Controllers/GorillaMovementController.cs
@@ -5,6 +5,8 @@ public class GorillaMovementController : Singleton<GorillaMovementController>  {
 
 	GameReferences gameRef;
 	private bool shouldAllowGorillaMovement;
+	private bool leftControlTapped;
+	private bool rightControlTapped;
 
 	public void Init(GameObject gameObject)
 	{
@@ -16,21 +18,38 @@ public class GorillaMovementController : Singleton<GorillaMovementController>  {
 	{
 		if (shouldAllowGorillaMovement)
 		{
-			int tapCount = Input.touchCount;
-			if (tapCount > 0) {
-				Touch touch = Input.GetTouch (0);
-				if (touch.phase == TouchPhase.Began || (Input.GetMouseButtonDown (0))) {
+			if ((Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) || (Input.GetMouseButtonDown (0))) {
 					
-					Vector3 worldPoint = Camera.main.ScreenToWorldPoint (touch.position);
-					GameObject leftMovementControl = gameRef.leftMovementControl;
-					GameObject rightMovementControl = gameRef.rightMovementControl;
-					if (leftMovementControl.GetComponent<Collider2D> ().OverlapPoint (worldPoint)) {
-						
-					} else if (leftMovementControl.GetComponent<Collider2D> ().OverlapPoint (worldPoint)) {
-						
-					}
-
+				Vector3 worldPoint = Vector3.zero;
+				#if UNITY_EDITOR
+				worldPoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				//for touch device
+				#elif (UNITY_ANDROID || UNITY_IPHONE || UNITY_WP8)
+				worldPoint = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+				#endif
+				GameObject leftMovementControl = gameRef.leftMovementControl;
+				GameObject rightMovementControl = gameRef.rightMovementControl;
+				if (leftMovementControl.GetComponent<Collider2D> ().OverlapPoint (worldPoint)) {
+					
+					leftControlTapped = true;
+					rightControlTapped = false;	
+					Debug.Log("Left Collidor tapped");
+					
+				} else if (rightMovementControl.GetComponent<Collider2D> ().OverlapPoint (worldPoint)) {
+					
+					leftControlTapped = false;
+					rightControlTapped = true;	
+					Debug.Log("Right Collidor tapped");
 				}
+
+			}
+
+			if ((leftControlTapped || rightControlTapped) && ((Input.touchCount > 0 && (Input.GetTouch (0).phase == TouchPhase.Moved || Input.GetTouch (0).phase == TouchPhase.Stationary))  || (Input.GetAxisRaw ("Mouse X") != 0 || Input.GetAxisRaw ("Mouse Y") != 0))) {
+					Debug.Log("Touch Moved");
+			}
+
+			if ((leftControlTapped || rightControlTapped) && ((Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Ended) || (Input.GetMouseButtonUp (0)))) {
+					Debug.Log("Touch Ended");
 			}
 
 //			for (int i = 0; i < tapCount; i++) 
