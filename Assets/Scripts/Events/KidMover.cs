b0671456d72115d4aa313f51cbefd2c06722b015
TAHOOR\HP <tahoor.pirzada@gmail.com>
2016-09-30 20:51:34 +0500

    Add Kids Animators

--- Assets/Scripts/Events/KidMover.cs
+++ Assets/Scripts/Events/KidMover.cs
@@ -2,13 +2,23 @@
 using System.Collections;
 
 public class KidMover : MonoBehaviour {
-
+    private GameReferences gameRef;
     private Vector3 targetPosition;
     private bool isFalling;
+    private Animator kidAnim;
 
 	void Start () {
+        kidAnim = GetComponent<Animator>();
         targetPosition = new Vector3(-transform.position.x, 0.0f, 0.0f);
         isFalling = false;
+        if(targetPosition.x > 0)
+        {
+            kidAnim.SetFloat("x", 1);
+        }
+        else if(targetPosition.x < 0)
+        {
+            kidAnim.SetFloat("x", -1);
+        }
 		StartCoroutine (FallKid ());
 	}
 
