a425bae6e23490e562d6ec6f57ed66aec3d65ee8
TAHOOR\HP <tahoor.pirzada@gmail.com>
2016-10-01 07:19:44 +0500

    Add Falling Kid State

--- Assets/Scripts/Events/KidMover.cs
+++ Assets/Scripts/Events/KidMover.cs
@@ -11,6 +11,7 @@ public class KidMover : MonoBehaviour {
         kidAnim = GetComponent<Animator>();
         targetPosition = new Vector3(-transform.position.x, 0.0f, 0.0f);
         isFalling = false;
+        kidAnim.SetBool("isFalling", false);
         if(targetPosition.x > 0)
         {
             kidAnim.SetFloat("x", 1);
@@ -26,6 +27,7 @@ public class KidMover : MonoBehaviour {
 	{
 		yield return new WaitForSeconds (Random.Range (1.0f, 7.0f));
 		isFalling = true;
+        kidAnim.SetBool("isFalling", true);
 		GetComponent<Rigidbody2D>().isKinematic = false;
 			
 	}
